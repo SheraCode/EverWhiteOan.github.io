9fe3bd7151d066304c10b047ba782db7f7bda953
Johannes Sipayung <johannesssipayung27@gmail.com>
2022-12-01 09:05:02 +0700

    loginsignup
    login dan sign up ok

---
+++ js/sticky_043.js
@@ -0,0 +1,22 @@
+/*
+Nama File : sticky_xxx.css
+Nama    :Samuel Volder
+NIM     :11422043
+Kelas   :41TRPL2
+*/
+jQuery(document).ready(function () {
+    var navOffset = jQuery("nav").offset().top;
+    jQuery("nav").wrap('<div class="nav-placeholder"></div>');
+    jQuery(".nav-placeholder").height(jQuery("nav").outerHeight());
+    jQuery("nav").wrapInner('<div class="nav-inner"></div>');
+    jQuery("nav-inner").wrapInner('<div class="nav-inner-most"></div>');
+    jQuery(window).scroll(function () {
+        var scrollPos = jQuery(window).scrollTop();
+        jQuery(".status").html(scrollPos);
+        if (scrollPos >= navOffset) {
+            jQuery("nav").addClass("fixed");
+        } else {
+            jQuery("nav").removeClass("fixed");
+        }
+    });
+});
---
+++ validate.js
@@ -0,0 +1,69 @@
+const BASE_URL = 'Login.html'
+
+function redirect(URI){
+    window.location.href = URI;
+}
+
+function formHandler(event, form) {
+    event.preventDefault();
+    validateForm(form);
+}
+
+function validateForm(form) {
+    var username = form["username"].value;
+    var email = form["email"].value;
+    var psword = form["psword"].value;
+    var confirm = form["confirm"].value;
+
+    if (username == "") {
+        alert("Username must be filled out");
+        return false;
+    }
+    if (email == "") {
+        alert("Email must be filled out");
+        return false;
+    }
+    if(!validateRegex()){
+        return false;
+    }
+    if (psword == "") {
+        alert("Password must be filled out");
+        return false;
+    }
+    if (confirm != psword) {
+        alert("Password tidak sama");
+        return false;
+    }
+    alert("Welcome! "+ username +" Will be redirected to "+" Login Page " );
+
+    // will be redirect in 3 seconds
+    setTimeout(redirect, 2000, BASE_URL);
+    progressBar(3);
+}
+
+function validateRegex(){
+    // remove border when typed
+    var email = document.getElementById("email").value;
+    var email2 = document.getElementById("email");
+    email2.addEventListener("keyup", function(){
+        email2.style.border = "greenyellow 3px solid";
+    });
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (re.test(email)){
+        return true;
+    } 
+    else {
+        email2.style.border = "red solid 3px";
+        alert("Email tidak valid");
+        return false;
+    } 
+}
+var progressBar = function(timeleft){
+    var downloadTimer = setInterval(function(){
+        if(timeleft <= 0){
+          clearInterval(downloadTimer);
+        }
+        document.getElementById("progressBar").value = 3 - timeleft + 1;
+        timeleft -= 1;
+      }, 1000);
+}
